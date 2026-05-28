c3c8181e201a4b2ee6468b61598498b01df514f4
Pavel Eliseev <forgenet@inbox.ru>
2026-05-28 15:03:32 +0300

    Add github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/queuing-flow/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
